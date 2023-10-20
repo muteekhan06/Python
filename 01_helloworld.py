550e253e88075b53eb36c4c4e49d5a4e15e0fc2c
Mutee ur Rehman <97782128+muteekhan06@users.noreply.github.com>
2023-10-20 14:56:43 +0500

    Update 01_helloworld.py

--- 01_helloworld.py
+++ 01_helloworld.py
@@ -1,17 +1,17 @@
 # This program prints Hello World
 # We will use print() function in this program. 
 # The print() function prints the specified message to the screen.
-# The message can be a string, or any other object.
-# The object will be converted into a string before written to the screen.
+# The message can be a string or any other object.
+# The object will be converted into a string before being written to the screen.
 
 print ("Hello World")
 
 # The syntax of print() is:
 # print(object(s), sep=separator, end=end, file=file, flush=flush)
 # Parameters in its syntax
-# In this syntax, object(s)	is any object. It will be converted to string before being printed.
-# Rest of the parameters are optional.
-# sep='separator', will specify how to separate the objects.Default is ' '.
-# end='end', will specify what to print at the end. Default is '\n'.
-# file,will spacisy an object with a write method. Default is sys.stdout
-# flush,will specify a Boolean, specifying if the output is flushed (True) or buffered (False). Default is False.
+# In this syntax, object(s)	is any object. It will be converted to a string before being printed.
+# The rest of the parameters are optional.
+# sep='separator', will specify how to separate the objects. The default is ' '.
+# end='end', will specify what to print at the end. The default is '\n'.
+# file, will specify an object with a write method. The default is sys. stdout
+# flush will specify a Boolean, specifying if the output is flushed (True) or buffered (False). The default is False.
