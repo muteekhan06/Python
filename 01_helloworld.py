fbc7e1f1ceecd67b898d767a288711cdeb18736f
Mutee ur Rehman <97782128+muteekhan06@users.noreply.github.com>
2023-10-20 14:54:57 +0500

    Update 01_helloworld.py

--- 01_helloworld.py
+++ 01_helloworld.py
@@ -1,7 +1,17 @@
+# This program prints Hello World
+# We will use print() function in this program. 
+# The print() function prints the specified message to the screen.
+# The message can be a string, or any other object.
+# The object will be converted into a string before written to the screen.
+
 print ("Hello World")
-print (2 + 3)
-print (2+3*43) 
-print ("My name is Mutee ur Rehman")
-print ('I am a computer science student')
-print("""This is a multi-line string.
-It contains multiple lines of text.""")
+
+# The syntax of print() is:
+# print(object(s), sep=separator, end=end, file=file, flush=flush)
+# Parameters in its syntax
+# In this syntax, object(s)	is any object. It will be converted to string before being printed.
+# Rest of the parameters are optional.
+# sep='separator', will specify how to separate the objects.Default is ' '.
+# end='end', will specify what to print at the end. Default is '\n'.
+# file,will spacisy an object with a write method. Default is sys.stdout
+# flush,will specify a Boolean, specifying if the output is flushed (True) or buffered (False). Default is False.
